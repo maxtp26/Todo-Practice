86f7601445b718bfbd97a14e3a9d42e123b92408
maxtp26 <maxtp26@gmail.com>
2026-01-30 13:28:54 -0500

    Refactored with OOP

--- todo_files.py
+++ todo_files.py
@@ -1,16 +1,39 @@
 import json
-def load_todos(filename):
-    try:
-        with open(filename, "r") as file:
-            return json.load(file)
-    except FileNotFoundError:
-        return [
+class TodoList:
+    def __init__(self, todo_list=None):
+        if todo_list is None:
+            self.todo_list = []
+        else:
+            self.todo_list = todo_list
+    def add(self, task, marked_done=False):
+        self.todo_list.append({"task": task, "done": marked_done})
+    def remove(self, index):
+        self.todo_list.pop(index)
+    def mark_done(self, index):
+        self.todo_list[index]["done"] = True
+    def mark_undone(self, index):
+        self.todo_list[index]["done"] = False
+    def list_all(self):
+        for index, item in enumerate(self.todo_list):
+            if item["done"]:
+                print(f"{index + 1}: [X] {item['task']}")
+            else:
+                print(f"{index + 1}: [_] {item['task']}")
+    def load(self, filename):
+        try:
+            with open(filename, "r") as file:
+                self.todo_list = json.load(file)
+        except FileNotFoundError:
+            self.todo_list = [
                 {"task": "Learn Python", "done": False},
                 {"task": "Build project", "done": False}
             ]
-def save_todos(todos, filename):
-    with open(filename, "w") as file:
-        json.dump(todos, file)
+    def save(self, filename):
+        with open(filename, "w") as file:
+            json.dump(self.todo_list, file)
+    def __len__(self):
+        return len(self.todo_list)
+
 def get_todo_index(todos, index_str):
     try:
         index = int(index_str) - 1
@@ -20,38 +43,12 @@ def get_todo_index(todos, index_str):
             return None
     except ValueError:
         return None
-def invalid_todo():
-    print("Invalid todo number!")
 def usage_error(command):
     print(f"Usage: {command} [number]")
-def add_todos(todos, task):
-    todos.append({"task": task, "done": False})
-def list_todos(todos):
-    for index, item in enumerate(todos):
-        if item["done"]:
-            print(f"{index + 1}: [X] {item['task']}")
-        else:
-            print(f"{index + 1}: [_] {item['task']}")
-def mark_done(todos, index_str):
-    index = get_todo_index(todos, index_str)
-    if index is not None:
-        todos[index]["done"] = True
-        return True  # success
-    return False  # failed
-def mark_undone(todos, index_str):
-    index = get_todo_index(todos, index_str)
-    if index is not None:
-        todos[index]["done"] = False
-        return True  # success
-    return False  # failed
-def remove_todo(todos, index_str):
-    index = get_todo_index(todos, index_str)
-    if index is not None:
-        todos.pop(index)
-        return True  # success
-    return False  # failed
+
 def main():
-    todos = load_todos("todos.json")
+    todos = TodoList()
+    todos.load("todos.json")
     while True:
         modified = False
         command = input("Command?: ").split(" ", 1)
@@ -61,39 +58,38 @@ def main():
         else:
             command_argument = None
         if command_input == "add":
-            add_todos(todos, command_argument)
+            todos.add(command_argument)
             print("Added item!")
             modified = True
         elif command_input == "done" or command_input == "check":
-            if command_argument is None:
-                usage_error("done")
-            elif mark_done(todos, command_argument):
+            index = get_todo_index(todos, command_argument)
+            if index is not None:
+                todos.mark_done(index)
                 print("Marked as done!")
                 modified = True
             else:
-                invalid_todo()
+                usage_error("done")
         elif command_input == "uncheck" or command_input == "undo":
-            if command_argument is None:
-                usage_error("undo")
-            elif mark_undone(todos, command_argument):
+            index = get_todo_index(todos, command_argument)
+            if index is not None:
+                todos.mark_undone(index)
                 print("Unmarked as done!")
                 modified = True
             else:
-                invalid_todo()
+                usage_error("undo")
         elif command_input == "remove":
-            if command_argument is None:
-                usage_error("remove")
-            elif remove_todo(todos, command_argument):
+            index = get_todo_index(todos, command_argument)
+            if index is not None:
+                todos.remove(index)
                 print("Removed todo!")
-                modified = True
             else:
-                invalid_todo()
+                usage_error("remove")
         elif command_input == "list":
-            list_todos(todos)
+            todos.list_all()
         elif command_input == "quit":
             break
         else:
             print("Invalid input!")
         if modified:
-            save_todos(todos, "todos.json")
+            todos.save("todos.json")
 main()
